c43cb876727b6b01ffaa075ed04256be36b23c5f
Beosti <ziroxismail@gmail.com>
2024-07-05 20:43:18 +0200

    updated to new version

--- server.py
+++ server.py
@@ -9,6 +9,21 @@ from dotenv import load_dotenv
 import os
 import modrinth
 import aiosqlite
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    json_stuff = jsonify(data)
+    print(json_stuff)
+    return jsonify(data), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
 
 intents = discord.Intents.all()
 
